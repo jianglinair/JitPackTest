10d1fce083e48d54817b57c3ace1338026e5bc77
jiang <jianglinair@163.com>
2021-02-01 11:26:46 +0800

    JitPack test

---
+++ libjl/src/main/java/com/jianglin/libjl/JitPackTest.java
@@ -0,0 +1,14 @@
+package com.jianglin.libjl;
+
+/**
+ * Author: Jianglin
+ * Time: 2021/2/1
+ * Description: todo
+ *
+ * @author jiang
+ */
+public class JitPackTest {
+    public static void test() {
+        System.out.println("This is from libjl.");
+    }
+}
